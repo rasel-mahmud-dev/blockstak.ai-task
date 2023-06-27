6c6e091ad5cab7ab9748de362713ac3cd1c2b406
Rasel Mahmud <rasel.mahmud.dev@gmail.com>
2023-06-27 16:01:30 +0600

    project initialized with nextjs

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="nav-logo">
+                    <img src="/NFters.svg" alt="logo"/>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Navigation;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Navigation from "@/app/components/Navigation";
+
+export default function Home() {
+  return (
+    <main>
+          <Navigation />
+      <div className="container">
+          sdfkljsdflkdsjf
+      </div>
+    </main>
+  )
+}
